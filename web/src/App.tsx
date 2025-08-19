e739b03ffd123a3105eb176bb65840e17ecd0e6b
tomato2718 <qweasdzxc030@gmail.com>
2025-08-19 16:18:02 +0800

    Feat: Initialize main page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+
 import { Layout } from "./layout";
+import init, { greet } from "./shared/PixelArtEditor";
 
 function App() {
+  useEffect(() => {
+    init().then();
+  }, []);
   return (
     <Layout>
-      <>app</>
+      <div className="w-full h-full flex items-center justify-center">
+        <button
+          className="px-4 py-2 rounded-xl bg-accent-9 hover:bg-accent-10 text-white cursor-pointer"
+          onClick={() => greet("World")}
+        >
+          Click me
+        </button>
+      </div>
     </Layout>
   );
 }
