dd22388f862ecd87c91cc25867e90b68c344f860
tomato2718 <qweasdzxc030@gmail.com>
2025-08-19 15:27:02 +0800

    Feat: Use dark mode as default

--- web/src/layout.tsx
+++ web/src/layout.tsx
@@ -1,6 +1,6 @@
 export function Layout({ children }: { children: React.ReactElement }) {
   return (
-    <div className="w-screen h-dvh bg-neutral-1 text-neutral-11">
+    <div className="w-screen h-dvh bg-neutral-1 text-neutral-11 dark">
       {children}
     </div>
   );
