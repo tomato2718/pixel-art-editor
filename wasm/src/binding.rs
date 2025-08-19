086bd887569bd78ba7753d455a13ec6c5bf6052d
tomato2718 <qweasdzxc030@gmail.com>
2025-08-19 20:16:37 +0800

    Feat: Create Web Assembly binding struct

--- wasm/src/binding.rs
+++ wasm/src/binding.rs
@@ -1,30 +1,39 @@
+use crate::canvas;
+use crate::pixel;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub struct Canvas {
+    canvas: canvas::Canvas,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {name}!"));
-}
-
-#[wasm_bindgen]
-pub struct Counter {
-    count: u32,
-}
-
-#[wasm_bindgen]
-impl Counter {
+impl Canvas {
     #[wasm_bindgen(constructor)]
-    pub fn new() -> Self {
-        Counter { count: 0 }
+    pub fn new(width: usize, height: usize) -> Self {
+        Canvas {
+            canvas: canvas::Canvas::new(width, height),
+        }
     }
 
     #[wasm_bindgen]
-    pub fn read(&mut self) -> u32 {
-        self.count += 1;
-        self.count
+    pub fn get_pixel(&self, x: usize, y: usize) -> Box<[u8]> {
+        let pixel = &self.canvas.get_pixel(x, y);
+
+        Box::new([pixel.r, pixel.g, pixel.b, pixel.a])
+    }
+
+    #[wasm_bindgen]
+    pub fn set_pixel(&mut self, x: usize, y: usize, pixel: &[u8]) {
+        self.canvas.set_pixel(
+            x,
+            y,
+            pixel::Pixel::new(pixel[0], pixel[1], pixel[2], pixel[3]),
+        );
+    }
+
+    #[wasm_bindgen]
+    pub fn to_buffer(&self) -> Box<[u8]> {
+        self.canvas.to_buffer().into_boxed_slice()
     }
 }
