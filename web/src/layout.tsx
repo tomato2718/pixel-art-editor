7f5e0c922730c8402451fbf88c257b7e99d5d8f8
tomato2718 <qweasdzxc030@gmail.com>
2025-08-20 17:01:31 +0800

    Feat: Update layout

--- web/src/layout.tsx
+++ web/src/layout.tsx
@@ -1,7 +1,16 @@
+import { Navigator } from "./widgets/Navigator";
+import { useTheme } from "./features/Theme";
+
 export function Layout({ children }: { children: React.ReactElement }) {
+  const { theme, switchTheme } = useTheme();
   return (
-    <div className="w-screen h-dvh bg-neutral-1 text-neutral-11 dark">
-      {children}
+    <div
+      className={`w-screen h-dvh bg-grayscale-1 text-grayscale-11 flex flex-col ${theme}`}
+    >
+      <div className="w-full">
+        <Navigator theme={theme} switchTheme={switchTheme} />
+      </div>
+      <div className="h-full overflow-y-auto">{children}</div>
     </div>
   );
 }
