eb6104d37f77360b1ac070990731addf72188b47
tomato2718 <qweasdzxc030@gmail.com>
2025-08-22 16:39:07 +0800

    Feat: Create Editor hooks

--- web/src/widgets/Editor/hooks.tsx
+++ web/src/widgets/Editor/hooks.tsx
@@ -1,4 +1,6 @@
-import { useState, useEffect, type RefObject } from "react";
+import { useState, useEffect, useCallback, type RefObject } from "react";
+
+import { type Canvas } from "@/shared/Canvas";
 
 type MousePosition = {
   x: number;
@@ -37,6 +39,7 @@ export function useMouseEvent(
   canvasRef: RefObject<HTMLCanvasElement | null>,
   mousePosition: MousePosition,
   pixelScale: number,
+  onClick: (x: number, y: number) => void,
 ) {
   const MOUSE_DEFAULT_ALPHA = 0.4;
   const x = pixelScale * (Math.ceil(mousePosition.x / pixelScale) - 1);
@@ -60,13 +63,67 @@ export function useMouseEvent(
       ctx.globalAlpha = 1;
       ctx.fillRect(x, y, pixelScale, pixelScale);
     };
+    const drawPixel = () => onClick(x, y);
 
     drawDefault();
     canvas.addEventListener("mousedown", drawFilled);
     canvas.addEventListener("mouseup", drawDefault);
+    canvas.addEventListener("mouseup", drawPixel);
     return () => {
       canvas.removeEventListener("mousedown", drawFilled);
       canvas.removeEventListener("mouseup", drawDefault);
+      canvas.removeEventListener("mouseup", drawPixel);
     };
-  }, [canvasRef, x, y, pixelScale]);
+  }, [canvasRef, x, y, pixelScale, onClick]);
+}
+
+export function useBackground(
+  canvasRef: RefObject<HTMLCanvasElement | null>,
+  editor: Canvas | null,
+  pixelScale: number,
+) {
+  useEffect(() => {
+    if (editor === null) return;
+
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let y = 0; y < editor.height(); y++) {
+      for (let x = 0; x < editor.width(); x++) {
+        const pixelColor = editor.get_pixel(x, y);
+        ctx.fillStyle = `rgba(${pixelColor[0]}, ${pixelColor[1]}, ${pixelColor[2]}, ${pixelColor[3]})`;
+        ctx.fillRect(x * pixelScale, y * pixelScale, pixelScale, pixelScale);
+      }
+    }
+  }, [canvasRef, editor, pixelScale]);
+}
+
+export function usePaintBrush(
+  editor: Canvas | null,
+  canvas: HTMLCanvasElement | null,
+  size: number,
+  color: { r: number; g: number; b: number; a: number },
+): (x: number, y: number) => void {
+  const setPixel = useCallback(
+    (x: number, y: number) => {
+      if (!editor || !canvas) return;
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+
+      ctx.fillStyle = `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a / 255})`;
+      ctx.fillRect(x, y, size, size);
+      editor.set_pixel(
+        x / size,
+        y / size,
+        new Uint8Array([color.r, color.g, color.b, color.a]),
+      );
+    },
+    [editor, canvas, size, color],
+  );
+  return setPixel;
 }
