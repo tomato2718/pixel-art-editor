0f9293d2422fa52067528cacb3a94c8f0f349520
tomato2718 <qweasdzxc030@gmail.com>
2025-08-25 15:28:46 +0800

    Chore: Build config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
